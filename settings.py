cd309df02316ce7846f7b398cafb4de4c17b4a2b
Adesina-T <89320825+Adesina-T@users.noreply.github.com>
2021-11-18 23:57:49 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                           ',
+'                           ',
+'                           ',
+' XX   XXX            XX    ',
+' XX                        ',
+'XXXXX       P   XX       XXXXXXX XCCXXX   XXXXXX',
+'XXXXX       XX                             CCCCCC',
+'XX     X   XXXX     XX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
+'       X   XXXXX    XX  XXX                             ',
+'    XXXX   XXXXX    XX  XXX',
+'XXXXXXXX   XXXXX    XX  XXX']
+
+tile_dimensions= 64
+screen_width= 1600
+screen_height = len(level_map) * tile_dimensions
